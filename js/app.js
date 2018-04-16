235097202d8ef4dc9b049c9133ea32e787f40051
weiliping <weiliping.cn@gmail.com>
2018-04-16 23:30:49 +0800

    update main_menu and profile page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,40 @@
+jQuery.fn.isChildOrSelf = function(obj) {
+  return (this.closest(obj).length > 0)  
+}
+
 $(document).ready(function(){
   set_section_height();
 });
 
 $(document).on('click', '#hamburger-menu', function(e){
-  if($(this).hasClass('open')){
-    $(this).removeClass('open');
+  if(!$(this).hasClass('open')){
+    show_menu($(this))
   }else{
-    $(this).addClass('open');
+    hide_menu($(this))  
   }
 });
+
+$(document).click(function(event){
+  if($('#hamburger-menu').hasClass('open')){
+    if(!$(event.target).isChildOrSelf('#main_menu_id') && !$(event.target).isChildOrSelf('#hamburger-menu')){
+      hide_menu($('#hamburger-menu'));
+    }
+  }
+});
+
+function hide_menu($obj){
+  $obj.removeClass('open');
+  $('#main_menu_id').css('left', '0px').animate({left: -320}, 350, function(){
+      $('#main_menu_id').hide();    
+      $('#main_menu_id').css('left', '');
+  });    
+}
+
+function show_menu($obj){
+  $obj.addClass('open');
+  $('#main_menu_id').show();
+  $('#main_menu_id').css('left', '-320px').animate({left: 0}, 350);  
+}
 
 function set_section_height(){
   var win_h = $(window).height();
