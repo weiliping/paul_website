857413b82ceebe4361e0e4bf905eb58f6698b128
weiliping <weiliping.cn@gmail.com>
2018-04-21 23:24:09 +0800

    skills section

--- js/app.js
+++ js/app.js
@@ -3,17 +3,8 @@ jQuery.fn.isChildOrSelf = function(obj) {
 }
 
 $(document).ready(function(){
-  // $(".regular").slick({
-  //   dots: false,
-  //   infinite: true,
-  //   slidesToShow: 2,
-  //   slidesToScroll: 2
-  // });
   set_section_height();
-
-  // $.get("json/data.json",{}, function(data){
-  //     console.log(" ---- data " + data.images[0].url )
-  // });
+  get_skills_content();
 });
 
 $(document).on('click', '#main_menu_id a', function(e){
@@ -54,4 +45,61 @@ function set_section_height(){
   var win_h = $(window).height();
   $('.header-content').height(win_h);
   //$('.main-content .sub-section .profile').height(win_h);
+}
+
+function get_skills_content(){
+  $.get("json/skills.json",{}, function(data){
+    $('#skills-table-id').html(get_html(data));
+  });
+}
+
+function get_html(data_obj){
+  var origin_html = '';
+  $.each(data_obj, function(index, obj_item){
+    origin_html += '<hr>';
+    origin_html += '<div class="sub-skills">';
+      origin_html += '<h6>' + obj_item.title + '</h6>';
+      origin_html += '<div class="section-content">';
+        origin_html += '<div class="section-row">';
+          origin_html += '<ul class="language">';
+          origin_html += get_sub_items(obj_item.data);
+          origin_html += '</ul>';
+          origin_html += '<div style="clear:both;"></div>';
+        origin_html += '</div>';
+      origin_html += '</div>';
+    origin_html += '</div>';
+  });
+  return origin_html;                
+}
+
+function get_sub_items(data_array){
+  var items_html = '';
+  $.each(data_array, function(index, obj){
+    items_html += '<li>'
+    if(obj.link != ''){
+      items_html += '<a class="skill-label" href="' + obj.link + '">'+ obj.name +'</a>'  
+    }else{
+      items_html += '<span class="skill-label">' + obj.name + '</span>';
+    }
+    items_html += get_stars(obj.star);
+    items_html += '</li>'
+  });
+  return items_html;  
+}
+function get_stars(star_num){
+  var star_html = '<span class="skill-star">';
+  var star_nums = star_num.split(".")
+  var total_star = parseInt(star_nums[0], 10);
+  for(var i=0; i < total_star; i++){
+    star_html += '<span class="icon-star-full"></span>';
+  }
+  if(star_nums[1] == '5'){
+    star_html += '<span class="icon-star-half"></span>';
+    total_star += 1;
+  }
+  for(var i=0; i < 5-total_star; i++){
+    star_html += '<span class="icon-star-empty"></span>';
+  }
+  star_html += '</span>';
+  return star_html;
 }
