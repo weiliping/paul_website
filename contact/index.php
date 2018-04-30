a1b5e0e6500056b65d965b83e7c5473e107f1f7c
weiliping <weiliping.cn@gmail.com>
2018-04-30 17:29:00 +0800

    enable the function of sending email

--- contact/index.php
+++ contact/index.php
@@ -1,19 +1,17 @@
 <?php
-    $to      = '30897292@qq.com';
+    $to      = 'paul@amazingwei.com';
     $subject = 'from paul website message';
-    $message = $_POST['msg']. '\r\n\r\n\r\n ---- name = ' . $_POST['name'] . '\r\n------ email = '. $_POST['email'];
+    $message = $_POST['msg']. "\r\n************ name = [" . $_POST['name'] . "] email = [". $_POST['email']."]";
     $headers = 'From: 30897292@qq.com' . "\r\n" .
         'Reply-To: 30897292@qq.com' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
-    mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message);
     $msg_json = ['name' => $_POST['name'], 'email' => $_POST['email'], 'msg' => $_POST['msg']];
 
     $file = 'contact.json';
     if (file_exists($file)) {
         $current = file_get_contents($file);
-        // Append a new person to the file
         $current .= json_encode($msg_json)."\n";
-        // Write the contents back to the file
         file_put_contents($file, $current);    
     } else {
         $myfile = fopen($file, "w") or die("Unable to open file!");
