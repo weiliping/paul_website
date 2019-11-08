ada98e6467ec77b42d0b605aff51e4a761342fdf
weiliping <weiliping.cn@gmail.com>
2019-11-08 23:47:24 +0800

    construct qr page css and js

--- js/qr.js
+++ js/qr.js
@@ -17,3 +17,45 @@ function show_spinner() {
 function hide_spinner() {
   $('#spinner-id').hide();
 }
+
+$(document).on('click', '#hamburger-menu', function (e) {
+  if (!$(this).hasClass('open')) {
+    show_menu($(this))
+  } else {
+    hide_menu($(this))
+  }
+});
+
+function hide_menu($obj) {
+  $obj.removeClass('open');
+  $('#main_menu_id').css({
+    left: '0px',
+    opacity: 1
+  }).animate({
+    left: -320,
+    opacity: 0
+  }, 350, function () {
+    $('#main_menu_id').hide();
+    $('#main_menu_id').css({
+      left: '',
+      opacity: 0
+    });
+  });
+}
+
+function show_menu($obj) {
+  $obj.addClass('open');
+  $('#main_menu_id').show();
+  $('#main_menu_id').css({
+    left: '-320px',
+    opacity: 0
+  }).animate({
+    left: 0,
+    opacity: 1
+  }, 350, function () {
+    $('#main_menu_id').css({
+      left: '0px',
+      opacity: 1
+    });
+  });
+}
